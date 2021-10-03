020bd2446142040e8ea1e568f44305ff7b79d94a
tajenderkumar5 <tajenderkumar50@gmail.com>
2021-10-03 13:51:32 +0530

    displaying database forms

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Todolist
 # Create your views here.
+
 def index(request):
-    return render(request,'todolist/index.html')
+    todo_items = Todolist.objects.order_by('id')
+    context ={'todo_items' : todo_items}
+    return render(request,'todolist/index.html',context)
 
