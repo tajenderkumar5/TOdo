d5cdea74cf02750edffc4cf59ea69cd017ddb603
tajenderkumar5 <tajenderkumar50@gmail.com>
2021-10-06 19:34:32 +0530

    add fom to capture data and delete taht data

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index,name='index')
+    path('', views.index,name='index'),
+    path('add', views.addTodoItem, name='add'),
+    path('completed/<todo_id>',views.completedTodo,name='completed'),
+    path('deletecompleted',views.deletecompleted,name='deletecompleted'),
+    path('deleteAll',views.deleteAll,name='deleteall'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from .models import Todolist
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context ={'todo_items' : todo_items}
+    form = TodoListForm()
+    context ={'todo_items' : todo_items, 'form' : form}
     return render(request,'todolist/index.html',context)
 
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
+
+def completedTodo(request,todo_id) :
+    todo=Todolist.objects.get(pk=todo_id)  
+    todo.completed= True
+    todo.save()
+
+    return redirect('index')
+
+def deletecompleted(request):
+    Todolist.objects.filter(completed__exact=True).delete()
+    return redirect('index')    
+
+def deleteAll(request):
+    Todolist.objects.all().delete()
+
+    return redirect('index')  
